d029dcc3d6a95d1694b07760edb481c278342f12
asher zeev aush <A0583251203@gmail.com>
2025-07-29 11:07:40 +0300

    Update notes and minor changes

--- Server_predictor.py
+++ Server_predictor.py
@@ -12,17 +12,20 @@ predictor = None
 @app.on_event("startup")
 def fetch_model_from_server1():
     global model, predictor
-    # Make a request to the training server to get the model
-    requests.get("http://server-train:8006/train")
-    if response.status_code == 200:
-        model_dict = response.json().get("dic")
-        if model_dict:
-            predictor = NaiveBayesPredictor(model_dict)
+    try:
+        # בצע את הבקשה לשרת המאמן
+        response = requests.get("http://server-train:8006/train")
+        if response.status_code == 200:
+            model_dict = response.json().get("dic")
+            if model_dict:
+                predictor = NaiveBayesPredictor(model_dict)
+                print("Model successfully fetched and predictor initialized.")
+            else:
+                print("Model dict not found in response!")
         else:
-            print("Model dict not found in response!")
-    else:
-        print("Failed to fetch model from Server 1")
-
+            print(f"Failed to fetch model from Server 1. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"Exception occurred while fetching model: {e}")
 @app.get("/predict")
 def predict(request: Request):
     # שלוף את כל הפרמטרים שהוזנו ב-URL
